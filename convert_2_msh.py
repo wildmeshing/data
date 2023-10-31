cbfcc8a571342299d667a8c36f34a5b8d8b388c9
Teseo Schneider <teseo@bluewin.ch>
2023-10-30 17:56:02 -0700

    convert tet meshes

--- convert_2_msh.py
+++ convert_2_msh.py
@@ -1,7 +1,6 @@
 import meshio as mio
 import glob
 import igl
-import os
 
 for ext in [".obj", ".ply", ".stl"]:
     for file in glob.glob(f"./**/*{ext}", recursive=True):
@@ -13,6 +12,25 @@ for ext in [".obj", ".ply", ".stl"]:
 
         m.write(
             file.replace(ext, ".msh"),
+            file_format="gmsh",
+            binary=True
+        )
+
+for ext in [".mesh"]:
+    for file in glob.glob(f"./**/*{ext}", recursive=True):
+        m = mio.read(file)
+        T = []
+        for c in m.cells:
+            if c.type == "tetra":
+                T = c.data
+                break
+        if len(T) == 0:
+            print(f"{file} doesnt contain any tets, skipping")
+            continue
+        m = mio.Mesh(m.points, [("tetra", T)])
+
+        m.write(
+            file.replace(ext, "_3d.msh"),
             file_format="gmsh",
             binary=True
         )
